e9e0ed9dbc1bd614c012092dbdebc69332cd35de
zeraus-co <zeraus-co@w2m.com>
2021-04-17 13:32:43 +0200

    Project creation

---
+++ src/main/java/com/w2m/zeraus/supher/SuperherosApplication.java
@@ -0,0 +1,13 @@
+package com.w2m.zeraus.supher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperherosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperherosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w2m/zeraus/supher/SuperherosApplicationTest.java
@@ -0,0 +1,13 @@
+package com.w2m.zeraus.supher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
